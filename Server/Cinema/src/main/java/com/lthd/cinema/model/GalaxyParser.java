f8ff21bc74c8f28ff77f9cc1217743a064acf74d
TranNam <trantunam.cqtk8@gmail.com>
2017-01-06 13:45:53 +0700

    update Galaxy parser

--- Server/Cinema/src/main/java/com/lthd/cinema/model/GalaxyParser.java
+++ Server/Cinema/src/main/java/com/lthd/cinema/model/GalaxyParser.java
@@ -43,47 +43,6 @@ public class GalaxyParser extends FilmParser{
 
     @Override
     public void parse() throws IOException {
-       /* Document doc = Jsoup.connect(url).get();
-        Elements elements = doc.select("li[class=col-4]");
-
-        for (int i = 0; i < elements.size(); i++) {
-        	String srcImageFilm = elements.get(i).child(0).child(0).attr("src");
-        	
-	        if (srcImageFilm.contains(" "))
-	            srcImageFilm = srcImageFilm.replaceAll(" ", "%20");
-
-	        filmImages.add(srcImageFilm);
-	        filmNames.add(elements.get(i).child(0).attr("title"));
-	        
-	        String urlShowTimes = elements.get(i).child(0).attr("href");
-
-	        Document doc2 = Jsoup.connect(urlShowTimes).get();
-	        Elements elements2 = doc2.select("strong").get(0).parent().parent().children();
-	        
-	        String info = "";
-            for (int j = 0; j < elements2.size(); j++)
-                if (elements2.get(j).nodeName().equals("p"))
-                    info += elements2.get(j).text() + "\n";
-
-            filmInfos.add(info);
-	        
-	        Elements titles = doc2.select("h2[class=silver-title]");
-            Elements content = doc2.select("div[class=silver-content]");
-            Map<String, String> showtime = new HashMap<String, String>();
-            for (int j = 0; j < titles.size(); j++) {
-            	if (titles.get(j).text().equals("Galaxy Bến Tre") 
-    	        		|| titles.get(j).text().equals("Galaxy Đà Nẵng") 
-    	        		|| titles.get(j).text().equals("Galaxy Mipec Long Biên"))
-    	        	continue;
-            	String time = "";
-                for (int k = 0; k < content.get(j).children().size(); k++) {
-                    time += content.get(j).child(k).child(0).text() + "\n";
-                    time += content.get(j).child(k).child(1).text() + "\n";
-                }
-                showtime.put(titles.get(j).text(), time);
-            }
-            showtimes.add(showtime);
-        }*/
     	
     	Element element = doc.select("div[class=sub-2]").get(0).child(0);
     	String ajaxFilm = element.attr("data-ajax");
